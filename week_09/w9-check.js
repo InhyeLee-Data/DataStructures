53d127c8eb5b604b5aec1340c4cc16f692006b52
Inhye <54472616+InhyeLee-Data@users.noreply.github.com>
2020-11-15 17:44:26 +0900

    Update w9-check.js

--- week_09/w9-check.js
+++ week_09/w9-check.js
@@ -15,28 +15,80 @@ const client = new Client(db_credentials);
 client.connect();
 
 // Sample SQL statements for checking your work: 
-var thisQuery = "SELECT * FROM sensorData;"; // print all values
-var secondQuery = "SELECT COUNT (*) FROM sensorData;"; // print the number of rows
-var thirdQuery = "SELECT sensorValue, COUNT (*) FROM sensorData GROUP BY sensorValue;"; // print the number of rows for each sensorValue
+var sensor_query1 = "SELECT * FROM sensorData;"; // print all values
+var sensor_query2 = "SELECT COUNT (*) FROM sensorData;"; // print the number of rows
+var sensor_query3 = "SELECT sensorValue, COUNT (*) FROM sensorData GROUP BY sensorValue;"; // print the number of rows for each sensorValue
+var phuket_query1 = "SELECT * FROM phuketData;";  // print all from phuket
+var phuket_query2 = "SELECT COUNT (*) FROM phuketData;"; // print the number of rows
+var phuket_query3 = "SELECT phuketTemp, COUNT (*) FROM phuketData GROUP BY phuketTemp ;"; 
+var seoul_query1 = "SELECT * FROM seoulData;";  // print all from seoul
+var seoul_query2 = "SELECT COUNT (*) FROM seoulData;"; // print the number of rows
+var seoul_query3 = "SELECT seoulTemp, COUNT (*) FROM seoulData GROUP BY seoulTemp ;"; 
 
-client.query(thisQuery, (err, res) => {
+
+client.query(sensor_query1, (err, res) => {
     if (err) {throw err}
     else {
     console.table(res.rows);
     }
 });
 
-client.query(secondQuery, (err, res) => {
+client.query(sensor_query2, (err, res) => {
     if (err) {throw err}
     else {
     console.table(res.rows);
     }
 });
 
-client.query(thirdQuery, (err, res) => {
+client.query(sensor_query3, (err, res) => {
     if (err) {throw err}
     else {
     console.table(res.rows);
     }
-    client.end();
+    
+});
+
+client.query(phuket_query1, (err, res) => {
+    if (err) {throw err}
+    else {
+    console.table(res.rows);
+    }
+   
+});
+
+client.query(phuket_query2, (err, res) => {
+    if (err) {throw err}
+    else {
+    console.table(res.rows);
+    }
+    
+});
+
+client.query(phuket_query3, (err, res) => {
+    if (err) {throw err}
+    else {
+    console.table(res.rows);
+    }
+});
+
+client.query(seoul_query1, (err, res) => {
+    if (err) {throw err}
+    else {
+    console.table(res.rows);
+    }
+});
+
+client.query(seoul_query2, (err, res) => {
+    if (err) {throw err}
+    else {
+    console.table(res.rows);
+    }
+});
+
+client.query(seoul_query3, (err, res) => {
+    if (err) {throw err}
+    else {
+    console.table(res.rows);
+    }
+    client.end(); // Only end at the last query
 });
